6368a4eae820d87f24fed652ec9d40b67572a759
Ananya Doshi <doshiananya2002@gmail.com>
2024-09-10 15:36:30 -0400

    first commit

---
+++ hw2_debugging.py
@@ -0,0 +1,82 @@
+# """Module providing a function printing python version."""
+# from rand import *
+
+
+# def merge_sort(arr):
+#     """Function printing python version."""
+#     if len(arr) == 1:
+#         return arr
+
+#     half = len(arr) // 2
+
+#     return recombine(merge_sort(arr[:half]), merge_sort(arr[half:]))
+
+
+# def recombine(left_arr, right_arr):
+#     """Function printing python version."""
+#     left_index = 0
+#     right_index = 0
+#     merge_arr = [None] * (len(left_arr) + len(right_arr))
+#     while left_index < len(left_arr) and right_index < len(right_arr):
+#         if left_arr[left_index] < right_arr[right_index]:
+#             right_index += 1
+#             merge_arr[left_index + right_index] = left_arr[left_index]
+#         else:
+#             left_index += 1
+#             merge_arr[left_index + right_index] = right_arr[right_index]
+
+#     for i in range(right_index, len(right_arr)):
+#         merge_arr[left_index + right_index] = right_arr[i]
+
+#     for i in range(left_index, len(left_arr)):
+#         merge_arr[left_index + right_index] = left_arr[i]
+
+#     return merge_arr
+
+
+# ARR = random_array([None] * 20)
+# arr_out = merge_sort(ARR)
+
+# print(arr_out)
+
+
+"""Module providing a function to perform merge sort on an array."""
+from rand import random_array
+
+
+def merge_sort(arr):
+    """Sorts an array using the merge sort algorithm."""
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return recombine(left, right)
+
+
+def recombine(left_arr, right_arr):
+    """Merges two sorted arrays into one sorted array."""
+    left_index, right_index = 0, 0
+    merge_arr = []
+
+    while left_index < len(left_arr) and right_index < len(right_arr):
+        if left_arr[left_index] < right_arr[right_index]:
+            merge_arr.append(left_arr[left_index])
+            left_index += 1
+        else:
+            merge_arr.append(right_arr[right_index])
+            right_index += 1
+
+    # Add remaining elements from both arrays
+    merge_arr.extend(left_arr[left_index:])
+    merge_arr.extend(right_arr[right_index:])
+
+    return merge_arr
+
+
+# Example usage
+ARR = random_array([0] * 20)
+arr_out = merge_sort(ARR)
+print(arr_out)
---
+++ rand.py
@@ -0,0 +1,20 @@
+import subprocess
+
+
+def random_array(arr):
+    shuffled_num = None
+    for i in range(len(arr)):
+        shuffled_num = subprocess.run(
+            ["shuf", "-i1-20", "-n1"], capture_output=True)
+        arr[i] = int(shuffled_num.stdout)
+    return arr
+
+
+# import random
+
+
+# def random_array(arr):
+#     """Fills the array with random integers between 1 and 20."""
+#     for i in range(len(arr)):
+#         arr[i] = random.randint(1, 20)
+#     return arr
